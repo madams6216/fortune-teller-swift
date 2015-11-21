d10159d1b1032ab9cd4d14671588d210727308b0
Michael Adams <madams6216@gmail.com>
2015-11-21 11:16:30 -0500

    updated version

--- FortuneTellerSwift/ViewController.swift
+++ FortuneTellerSwift/ViewController.swift
@@ -1,25 +1,20 @@
-//
-//  ViewController.swift
-//  FortuneTellerSwift
-//
-//  Created by michael adams on 11/21/15.
-//  Copyright © 2015 michael adams. All rights reserved.
-//
-
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var askView: UIImageView!
+    
+    @IBOutlet var askLabel: UIView!
+    
+    @IBOutlet weak var askButton: UIButton!
+    
+    @IBOutlet weak var askText: UITextField!
+    
+    @IBAction func pressedButton(sender: AnyObject) {
+        print("button pressed")
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
-
 }
-
