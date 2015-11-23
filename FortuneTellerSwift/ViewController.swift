31bb18ce1d64c5a7a99e9b3a9480cfd7383c6948
Michael Adams <madams6216@gmail.com>
2015-11-23 14:10:48 -0500

    created app

--- FortuneTellerSwift/ViewController.swift
+++ FortuneTellerSwift/ViewController.swift
@@ -4,17 +4,47 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var askView: UIImageView!
     
-    @IBOutlet var askLabel: UIView!
+    @IBOutlet weak var questionLabel: UILabel!
+   
+    @IBOutlet weak var askLabel: UILabel!
     
     @IBOutlet weak var askButton: UIButton!
     
-    @IBOutlet weak var askText: UITextField!
+    @IBOutlet weak var askTextField: UITextField!
     
     @IBAction func pressedButton(sender: AnyObject) {
+        
         print("button pressed")
+        
+       askLabel.text = randomText()
+        
+       askTextField.text = ""
+        
+        
     }
 
+
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
+        
+
+        
+    }
+    
+    
+    func randomText() -> String {
+        let words = ["Yes", "No", "The end is near","Absolutely", "Cannot predict now", "Prospect good", "Very doubtful", "Without a doubt", "Outlook not so good", "Don't count on it", "Cannot predict now"]
+        return words[Int(arc4random_uniform(UInt32(words.count)))]
     }
 }
