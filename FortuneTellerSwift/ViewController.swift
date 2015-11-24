85416ca876746461d51be0a48a36e5ce74976578
Michael Adams <madams6216@gmail.com>
2015-11-24 10:14:10 -0500

    Added a timer

--- FortuneTellerSwift/ViewController.swift
+++ FortuneTellerSwift/ViewController.swift
@@ -15,36 +15,39 @@ class ViewController: UIViewController {
     @IBAction func pressedButton(sender: AnyObject) {
         
         print("button pressed")
+       
+        askLabel.text = randomText()
         
-       askLabel.text = randomText()
+        
+            
+        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(2 * Double(NSEC_PER_SEC)))
+        dispatch_after(time, dispatch_get_main_queue(), {
+            self.askLabel.text = ""
+        })
+        
         
        askTextField.text = ""
         
         
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+
     }
-
-
+    
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
-        view.addGestureRecognizer(tap)
-    }
-    
-    //Calls this function when the tap is recognized.
-    func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
-        view.endEditing(true)
         
-
         
     }
     
     
     func randomText() -> String {
-        let words = ["Yes", "No", "The end is near","Absolutely", "Cannot predict now", "Prospect good", "Very doubtful", "Without a doubt", "Outlook not so good", "Don't count on it", "Cannot predict now"]
+        let words = ["Yes", "No", "The end is near","Absolutely", "Cannot predict now", "Prospect good", "Very doubtful", "Without a doubt", "Outlook not so good", "Don't count on it", "Cannot predict now", "Ask again later", "Signs point to yes", "No Doubt about it"]
         return words[Int(arc4random_uniform(UInt32(words.count)))]
     }
+    
+    
 }
