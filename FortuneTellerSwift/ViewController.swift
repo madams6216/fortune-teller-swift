f0b604d2132e18e37a8d3ad01d44573c509c2a65
Michael Adams <madams6216@gmail.com>
2015-11-24 10:37:17 -0500

    Fixed the keyboard function

--- FortuneTellerSwift/ViewController.swift
+++ FortuneTellerSwift/ViewController.swift
@@ -29,9 +29,7 @@ class ViewController: UIViewController {
        askTextField.text = ""
         
         
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
-        view.addGestureRecognizer(tap)
-
+        
     }
     
     
@@ -39,8 +37,20 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
+        view.addGestureRecognizer(tap)
+    }
+    
+    //Calls this function when the tap is recognized.
+    func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
         
-        
+    }
+    
+    
+
+    
     }
     
     
@@ -50,4 +60,3 @@ class ViewController: UIViewController {
     }
     
     
-}
