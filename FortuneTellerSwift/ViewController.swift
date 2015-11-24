4945a777a00683d812d2a630ddac61f28c911c60
Michael Adams <madams6216@gmail.com>
2015-11-24 17:08:40 -0500

    shows message to user if no text is entered

--- FortuneTellerSwift/ViewController.swift
+++ FortuneTellerSwift/ViewController.swift
@@ -14,28 +14,45 @@ class ViewController: UIViewController {
     
     @IBAction func pressedButton(sender: AnyObject) {
         
+        // what do we want to do?
+        // we want to ...
+        // when a user taps the button, we want to check if the textbox is empty or not
+        // and if it is empty, we want to show them a message
+        
+//        if textBoxIsEmpty {
+//            showMessage
+//        } else {
+//            showAnswer
+//        }
+        
         print("button pressed")
-       
-        askLabel.text = randomText()
         
-        
+        if (askTextField.text!.isEmpty) {
+            let alert = UIAlertView()
+            alert.title = "No Text"
+            alert.message = "Please Enter Text In The Box"
+            alert.addButtonWithTitle("Ok")
+            alert.show()
+        } else {
+            askLabel.text = randomText()
             
-        let time = dispatch_time(DISPATCH_TIME_NOW, Int64(3 * Double(NSEC_PER_SEC)))
-        dispatch_after(time, dispatch_get_main_queue(), {
-            self.askLabel.text = ""
-        })
-        
-        
-       askTextField.text = ""
-        
-        
-        
+            let time = dispatch_time(DISPATCH_TIME_NOW, Int64(3 * Double(NSEC_PER_SEC)))
+            dispatch_after(time, dispatch_get_main_queue(), {
+                self.askLabel.text = ""
+            })
+            
+            askTextField.text = ""
+        }
+
     }
+    
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
         
         
         
@@ -52,12 +69,6 @@ class ViewController: UIViewController {
     }
     
     
-
-    
-    }
-
-
-    
     func randomText() -> String {
         let words = ["Yes", "No", "The end is near","Absolutely", "Cannot predict now", "Prospect good", "Very doubtful", "Without a doubt", "Outlook not so good", "Don't count on it", "Cannot predict now", "Ask again later", "Signs point to yes", "No Doubt about it"]
         return words[Int(arc4random_uniform(UInt32(words.count)))]
@@ -66,4 +77,12 @@ class ViewController: UIViewController {
         
     }
 
+
     
+    
+        
+    
+    
+
+}
+     
