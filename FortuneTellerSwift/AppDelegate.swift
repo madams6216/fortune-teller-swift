dadd20432d14c510b393681be155d66ced1acf3c
Michael Adams <madams6216@gmail.com>
2015-11-24 21:45:08 -0500

    fixed keyboard issue

--- FortuneTellerSwift/AppDelegate.swift
+++ FortuneTellerSwift/AppDelegate.swift
@@ -13,10 +13,21 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
+    
+    
+    
+    
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
         return true
+        
+        
+        
+        
+        
+        
+        
     }
 
     func applicationWillResignActive(application: UIApplication) {
--- FortuneTellerSwift/ViewController.swift
+++ FortuneTellerSwift/ViewController.swift
@@ -52,31 +52,31 @@ class ViewController: UIViewController {
     
     
     
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    func textFieldShouldReturn(textField: UITextField!) -> Bool {
+        self.view.endEditing(true);
+        return false;
         
         
-        
-        
-        
-        
-        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: "dismissKeyboard")
-        view.addGestureRecognizer(tap)
+        func textFieldDoneEditing(sender: UITextField) {
+            sender.resignFirstResponder()}
     }
     
-    //Calls this function when the tap is recognized.
-    func dismissKeyboard() {
-        //Causes the view (or one of its embedded text fields) to resign the first responder status.
-        view.endEditing(true)
-        
-    }
+    
     
     
     func randomText() -> String {
         let words = ["Yes", "No", "The end is near","Absolutely", "Cannot predict now", "Prospect good", "Very doubtful", "Without a doubt", "Outlook not so good", "Don't count on it", "Cannot predict now", "Ask again later", "Signs point to yes", "No Doubt about it"]
         return words[Int(arc4random_uniform(UInt32(words.count)))]
         
+        func textFieldShouldReturn(textField: UITextField!) -> Bool {
+            self.view.endEditing(true);
+            return false;
+            
+            
+                    }
+        
+
+        
         
         
     }
@@ -87,6 +87,6 @@ class ViewController: UIViewController {
         
     
     
-
 }
+
      
